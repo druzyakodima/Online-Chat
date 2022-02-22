bb2d90654c63684d8990b345013180af13d2587b
Dima <dimkadruz@icloud.com>
2022-02-22 22:58:28 +0300

    Домашка 12 готова!

--- src/main/java/com/example/windowchatlesson4/HelloController.java
+++ src/main/java/com/example/windowchatlesson4/HelloController.java
@@ -1,17 +1,13 @@
 package com.example.windowchatlesson4;
 
-import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.stage.Stage;
+
 
 
 public class HelloController {
