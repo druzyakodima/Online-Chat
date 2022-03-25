41e9e060054fecc49d76d530c1ba7560ac8a62c6
druzyakodima <92546554+druzyakodima@users.noreply.github.com>
2022-03-25 18:21:39 +0300

    Update ChatController.java

--- src/main/java/com/example/windowchatlesson4/controllers/ChatController.java
+++ src/main/java/com/example/windowchatlesson4/controllers/ChatController.java
@@ -165,13 +165,6 @@ public class ChatController {
     public void addUserList(String[] usernames) {
 
         Arrays.sort(usernames);
-
-        for (int i = 0; i < usernames.length; i++) {
-            if (usernames[i].equals(netWork.getUsername())) {
-                usernames[i] = usernames[i];
-            }
-        }
-
         userList.getItems().clear();
         Collections.addAll(userList.getItems(), usernames);
     }
