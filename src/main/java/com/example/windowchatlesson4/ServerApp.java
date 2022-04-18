07546a09912ed3941f910885e558724fad4543e7
Dima <dimkadruz@icloud.com>
2022-04-18 13:24:33 +0300

    Добавил логирование.

--- src/main/java/com/example/windowchatlesson4/ServerApp.java
+++ src/main/java/com/example/windowchatlesson4/ServerApp.java
@@ -10,6 +10,7 @@ public class ServerApp {
     private static final int SERVER_PORT = 8180;
 
     public static void main(String[] args) {
+
         PropertyConfigurator.configure("src/main/resources/logs/configs/log4j.properties");
         try {
             new EchoServer(SERVER_PORT).start();
